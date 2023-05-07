bc9ccd7c2306e30e4970c2e62a2278585702ba5e
Franklin Mensah <fiificode@gmail.com>
2023-05-07 19:40:13 +0000

    designing of auth pages

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import AuthForm from "./components/AuthForm";
 
 export default function Home() {
   return (
@@ -11,12 +12,12 @@ export default function Home() {
       className="mx-auto w-auto"
       src={"./images/logo.png"}
        />
-       <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+       <h2 className="mt-3 text-center text-3xl font-bold tracking-tight text-gray-900">
         Sign in to your account
        </h2>
     </div>
     {/* Auth form  */}
-    
+    <AuthForm />
    </div>
   )
 }
